b363aad58ad4ce5342c3e9ddaaff895f3f7b5a28
Artem <119800207+artemivanichenko@users.noreply.github.com>
2025-08-18 12:20:43 +0200

    fix App

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "@styled/modern-normalize";
